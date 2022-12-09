3c95c76cda678929389ac4a9dbc27136d547cf9e
Yash Mishra <ymishra502@gmail.com>
2022-12-09 10:41:57 +0530

    Added Game.py

---
+++ Game.py
@@ -0,0 +1,9 @@
+import random
+num=random.random()
+num2=random.random()
+if num==num2:
+    print("Congratulations, You have used all your luck on this insignificant lottery")
+else:
+    print("Better luck next time")
+print(num)
+print(num2)
